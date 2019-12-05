830fd315b1dd0cd7faf1238493949b9fb0d2f9c6
PSheon <pauljiang61020@gmail.com>
2019-12-05 16:40:07 +0800

    Site updated: 2019-12-05 16:40:02

--- js/obsidian.js
+++ js/obsidian.js
@@ -1259,5 +1259,5 @@ $(function () {
   utiliseBgColor();
   initialTyped();
   Obsidian.setCodeRowWithLang();
-  console.log("%c Github %c", "background:#24272A; color:#73ddd7", "", "https://github.com/TriDiamond/hexo-theme-obsidian");
+  console.log("%c Very Thanks to TriDiamond Github %c", "background:#24272A; color:#73ddd7", "", "https://github.com/TriDiamond/hexo-theme-obsidian");
 });
